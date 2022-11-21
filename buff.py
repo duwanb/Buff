a0811211abf8f61311dc84fd623de7f0ce6ce2a5
Duwan Bassani <duwan55@yahoo.com.br>
2022-11-20 23:13:57 -0300

    Feito

--- buff.py
+++ buff.py
@@ -3,7 +3,7 @@ from selenium.webdriver.chrome.options import Options as ChromeOptions
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-
+import time
 
 #for mac
 #chrome_path = r'/usr/local/bin/chromedriver' #path from 'which chromedriver'
@@ -11,7 +11,7 @@ chrome_path = r'./chromedriver' #path from 'which chromedriver'
 
 options = ChromeOptions()
 options.add_extension('./Buff-Utility.crx')
-options.headless = True
+#options.headless = True
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 #driver = webdriver.Chrome(executable_path=chrome_path, options=options) 
 # url of google news website
@@ -26,12 +26,20 @@ listacompleta=[]
 # to open the url in the browser
 driver.get(url)  
 items = driver.find_elements(By.CLASS_NAME, "market-card")
+html = driver.page_source
+
+time.sleep(5)
+
+
 
 for a in items:
     filter = a.find_elements(By.TAG_NAME, "li")
     for b in filter:
         nomes = a.find_elements(By.TAG_NAME, "h3")
         precos = a.find_elements(By.TAG_NAME, "strong")
+
+for a in nomes:
+    precomenor = a.find_elements(By.TAG_NAME, "span")
         #precos = a.find_elements(By.CSS_SELECTOR, "grid-column")
         #<span style="color: #eea20e;font-weight: 700;"
 for a in nomes:
@@ -52,5 +60,7 @@ print("\n\n\n\n\n")
 for a in listacompleta:
     print(a)
 
-
+with open('./html.txt', 'w') as f:
+    f.write(html)
+    
 driver.quit()
